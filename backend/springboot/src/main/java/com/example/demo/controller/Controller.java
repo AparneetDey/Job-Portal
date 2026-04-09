e3d48fda7049dd1f295df2ad33d7a8b6afed0eef
Aparneet Dey <aparnitd@gmail.com>
2026-04-09 16:23:37 +0530

    feat: initialize Spring Boot backend project with MySQL connectivity and basic controller endpoints

--- backend/springboot/src/main/java/com/example/demo/controller/Controller.java
+++ backend/springboot/src/main/java/com/example/demo/controller/Controller.java
@@ -1,10 +1,20 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 @RestController
 public class Controller {
+
+  @Autowired
+  private DataSource dataSource;
 
   @GetMapping("/")
   public String Welcome() {
@@ -12,8 +22,15 @@ public class Controller {
   }
 
   @GetMapping("/about")
-  public String About() {
-    return "About Page";
+  public String About() throws SQLException {
+    return "About Page" + dataSource.getConnection();
+  }
+
+  @PostMapping("/login")
+  public String LoginUser(@RequestBody String entity) {
+    // TODO: process POST request
+    System.out.println(entity);
+    return entity;
   }
 
 }
